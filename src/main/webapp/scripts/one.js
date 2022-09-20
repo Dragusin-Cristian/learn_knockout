70ed2388695fcfc51d31f8a0d5a3d52c204dab1b
Cristian Dragusin <cristian.dragusin@microfocus.com>
2022-09-20 13:50:55 +0300

    local state logic

--- src/main/webapp/scripts/one.js
+++ src/main/webapp/scripts/one.js
@@ -5,7 +5,24 @@ function CounterViewModel() {
     //* (just like useState)
     self.firstName = ko.observable("Wayne")
 
+    //* set the value
     self.firstName("George")
+
+    //* get the value
+    // self.firstName()
+
+    self.count = ko.observable(0)
+    self.increase = function(){
+        var currentValue = self.count()
+        self.count(currentValue+1)
+    }
+
+    self.decrease = function(){
+        if(self.count() > 0){
+            var currentValue = self.count()
+        self.count(currentValue-1)
+        }
+    }
 }
 
 ko.applyBindings(new CounterViewModel(),
